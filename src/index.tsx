f510958765db1b57fa9acb601d4c59f0bef499cb
Oleksii <AlexTurovets1999@gmail.com>
2023-01-22 12:20:47 +0200

    import learned

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function List() {
-    return (
-        <React.Fragment>
-            <ul>
-                <li>List item 1</li>
-                <li>List item 2</li>
-                <li>List item 3</li>
-            </ul>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-                ipsum voluptas quasi hic minima iusto ex, vero dignissimos
-                beatae suscipit tempora dicta, id facere officia ipsam eligendi.
-                Culpa, sapiente excepturi.
-            </p>
-        </React.Fragment>
-    );
-}
+import { List } from "./List";
 
 type TitleProps = {
     name: string;
