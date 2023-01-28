5fd020948d070c444916fc7784e85e7e1e9bf6d0
Oleksii <AlexTurovets1999@gmail.com>
2023-01-28 11:02:32 +0200

    modified module css

--- src/List.tsx
+++ src/List.tsx
@@ -2,7 +2,7 @@ import classes from './List.module.css'
 
 type Props = {};
 console.log(classes)
-const {content, test, articleDesc } = classes
+const {content, test, ['article-desc']:articleDesc } = classes
 
 export const List = (props: Props) => {
     let data = true
@@ -13,7 +13,7 @@ export const List = (props: Props) => {
                 <li>List item 2</li>
                 <li>List item 3</li>
             </ul>
-            <p className={` ${classes["articleDesc"]} ${data ? content : test}`}>
+            <p className={` ${classes["article-desc"]} ${data ? content : test}`}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
                 ipsum voluptas quasi hic minima iusto ex, vero dignissimos
                 beatae suscipit tempora dicta, id facere officia ipsam eligendi.
