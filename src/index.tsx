9b4b7521d2bc0c1ede0256678169cb6aea26af14
Oleksii <AlexTurovets1999@gmail.com>
2023-01-28 10:18:05 +0200

    added unnesessary param

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import { List } from "./List";
 
 type TitleProps = {
-    name: string;
-    text?: string;
-    year?: number;
+    name?: string;
+    text: string;
+    year: number;
 };
 
-function Title({ name, text, year }: TitleProps) {
+function Title({ name= 'Jack', text, year }: TitleProps) {
     return (
         <>
             <h1>Hello {name}</h1>
@@ -21,7 +21,10 @@ function Title({ name, text, year }: TitleProps) {
 const App = () => {
     return (
         <div className="app">
-            <Title name="App" text="Lorem ipsum dolor" year={1970} />
+            <Title
+                // name="App"
+                text="Lorem ipsum dolor"
+                year={1970} />
             <Title
                 name="React"
                 text="sit amet consectetur adipisicing"
