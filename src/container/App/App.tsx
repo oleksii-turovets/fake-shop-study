7f4c4c049bbee12f6dad90783fd24ae0e9c5f5fd
Oleksii <AlexTurovets1999@gmail.com>
2023-01-28 11:51:01 +0200

    add Header, Main, Footer

---
+++ src/container/App/App.tsx
@@ -0,0 +1,18 @@
+import Footer from "container/Footer/Footer"
+import Header from "container/Header/Header"
+import Main from "container/Main/Main"
+
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+      <>
+          <Header />
+          <Main />
+          <Footer />
+      </>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { List } from "./List";
-
-type TitleProps = {
-    name?: string;
-    text: string;
-    year: number;
-};
-
-function Title({ name= 'Jack', text, year }: TitleProps) {
-    return (
-        <>
-            <h1>Hello {name}</h1>
-            <p>{text}</p>
-            <div>Year: {year}</div>
-        </>
-    );
-}
-
-const App = () => {
-    return (
-        <div className="app">
-            <Title
-                text="Lorem ipsum dolor"
-                year={1970} />
-            <Title
-                name="React"
-                text="sit amet consectetur adipisicing"
-                year={1990}
-            />
-            <Title
-                name="TS"
-                text="id facere officia ipsam eligendi"
-                year={2015}
-            />
-            <List />
-        </div>
-    );
-};
+import App from "./container/App/App";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
