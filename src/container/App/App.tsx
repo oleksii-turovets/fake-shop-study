14687c89bade2e1d9ce01ab4eb28ea46cd56208a
Oleksii <AlexTurovets1999@gmail.com>
2023-01-29 10:52:13 +0200

    mui and mui bar added

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,6 +1,7 @@
 import Footer from "container/Footer/Footer"
 import Header from "container/Header/Header"
 import Main from "container/Main/Main"
+import CssBaseline from "@mui/material/CssBaseline"
 
 
 type Props = {}
@@ -8,6 +9,7 @@ type Props = {}
 const App = (props: Props) => {
   return (
       <>
+          <CssBaseline />
           <Header />
           <Main />
           <Footer />
