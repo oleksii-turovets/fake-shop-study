3947f38b102b79c8dfc374dd7206bd510a978069
Oleksii <AlexTurovets1999@gmail.com>
2023-01-29 11:07:28 +0200

    style priority fixed

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,20 +1,20 @@
-import Footer from "container/Footer/Footer"
-import Header from "container/Header/Header"
-import Main from "container/Main/Main"
-import CssBaseline from "@mui/material/CssBaseline"
+import Footer from "container/Footer/Footer";
+import Header from "container/Header/Header";
+import Main from "container/Main/Main";
+import CssBaseline from "@mui/material/CssBaseline";
+import { StyledEngineProvider } from "@mui/material/styles";
 
-
-type Props = {}
+type Props = {};
 
 const App = (props: Props) => {
-  return (
-      <>
-          <CssBaseline />
-          <Header />
-          <Main />
-          <Footer />
-      </>
-  )
-}
+    return (
+        <StyledEngineProvider injectFirst>
+            <CssBaseline />
+            <Header />
+            <Main />
+            <Footer />
+        </StyledEngineProvider>
+    );
+};
 
-export default App
+export default App;
