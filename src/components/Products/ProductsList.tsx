02a8e529d7e3c281db9a7e4dce777b2f376cef32
Oleksii <AlexTurovets1999@gmail.com>
2023-02-04 11:52:31 +0200

    props items added

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -8,15 +8,33 @@ const ProductsList = (props: Props) => {
             <Typography variant="h3" component="h2" align="center">
                 List of Products
             </Typography>
-            <Grid container spacing={4}>
+            <Grid container spacing={4} alignItems="stretch">
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 14 Pro"
+                        description="This is the newest iPhone in the world"
+                        type="phone"
+                        capacity="256"
+                        price={500}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 13 Pro Max"
+                        description="This is stil nice model"
+                        type="phone"
+                        capacity="512"
+                        price={450}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 12"
+                        description="This is the old one. Really, do u want this?"
+                        type="phone"
+                        capacity="128"
+                        price={300}
+                    />
                 </Grid>
             </Grid>
         </>
