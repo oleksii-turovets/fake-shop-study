72571212bf54cdfc7e1d621bc237e2ff2761e1d8
Oleksii <AlexTurovets1999@gmail.com>
2023-02-05 11:10:28 +0200

    counter structure added

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -1,4 +1,10 @@
-import { Button, Card, CardActions, CardContent } from "@mui/material"
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    TextField,
+} from "@mui/material";
 import "./ProductsListItem.scss";
 
 type Props = {
@@ -15,17 +21,24 @@ const ProductsListItem = ({
     type,
     capacity,
     price,
-    image
+    image,
 }: Props) => {
     return (
         <Card className="product" variant="outlined">
             <CardContent>
-                <div className="product-image"><img src={image} alt="" /></div>
+                <div className="product-image">
+                    <img src={image} alt="" />
+                </div>
                 <div className="product-title">{title}</div>
                 <div className="product-description">{description}</div>
                 <div className="product-features">Type: {type}</div>
                 <div className="product-features">Capacity: {capacity} Gb</div>
                 <div className="product-price">{price}$</div>
+                <div className="product-quantity">
+                    <Button variant="outlined">-</Button>
+                    <TextField size="small" value="1" />
+                    <Button variant="outlined">+</Button>
+                </div>
             </CardContent>
             <CardActions className="btns-wrap">
                 <Button variant="outlined">Add to cart</Button>
@@ -33,4 +46,4 @@ const ProductsListItem = ({
         </Card>
     );
 };
-export default ProductsListItem
+export default ProductsListItem;
