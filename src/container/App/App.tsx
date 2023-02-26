a4dc0f71a73f28c9531f81763e683c3fed1ca5d1
Oleksii <AlexTurovets1999@gmail.com>
2023-02-26 11:57:05 +0200

    links added

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,9 +1,12 @@
 import Footer from 'container/Footer/Footer'
 import Header from 'container/Header/Header'
-import Main from 'container/Main/Main'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
 import { useState } from 'react'
+import { Container } from '@mui/material'
+import { Route, Routes } from 'react-router-dom'
+import Home from 'pages/Home/Home'
+import CartPage from 'pages/CartPage/CartPage'
 
 type Props = {}
 
@@ -28,7 +31,22 @@ const App = (props: Props) => {
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header productsInCart={productsInCart} />
-            <Main addProductToCart={addProductToCart} />
+            <Container
+                sx={{
+                    padding: '60px 0',
+                }}
+            >
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home addProductToCart={addProductToCart} />}
+                    />
+                    <Route
+                        path="/cart"
+                        element={<CartPage />}
+                    />
+                </Routes>
+            </Container>
             <Footer />
         </StyledEngineProvider>
     )
