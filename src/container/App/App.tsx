46b3675f8f9033a27dfdeaecf73e5343e805b9e8
Oleksii <AlexTurovets1999@gmail.com>
2023-02-26 10:27:52 +0200

    commit before new task

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -28,9 +28,6 @@ const App = (props: Props) => {
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header productsInCart={productsInCart} />
-            <button onClick={() => addProductToCart(2, 5)}>
-                Add to cart (id:2, count:5)
-            </button>
             <Main addProductToCart={addProductToCart} />
             <Footer />
         </StyledEngineProvider>
