f937bdb634f5006e89c86010fd10a91299fd31e3
Oleksii <AlexTurovets1999@gmail.com>
2023-03-12 11:59:13 +0200

    added slice method functions

--- src/redux/likeReducer.ts
+++ src/redux/likeReducer.ts
@@ -9,25 +9,36 @@ export const initialState: ProductsLike = {
     2: true,
 }
 
-// export const likeSlice = createSlice({
-//     name: 'like',
-//     initialState,
-//     reducers:{}
-// })
-
-// export default likeSlice.reducer
-
-const likeReducer = (state = initialState, action: AnyAction) => {
-    switch (action.type) {
-        case 'TOGGLE_LIKE': {
-            return {
-                ...state,
-                [action.id]: !state[action.id],
-            }
-        }
-        default:
-            return state
+export const likeSlice = createSlice({
+    name: 'like',
+    initialState,
+    reducers: {
+        addLike: (state, action) => ({
+            ...state,
+            [action.payload]: true,
+        }),
+        removeLike: (state, action) => ({
+            ...state,
+            [action.payload]: false,
+        }),
     }
-}
+})
 
-export default likeReducer
+export const {addLike, removeLike} = likeSlice.actions
+
+export default likeSlice.reducer
+
+// const likeReducer = (state = initialState, action: AnyAction) => {
+//     switch (action.type) {
+//         case 'TOGGLE_LIKE': {
+//             return {
+//                 ...state,
+//                 [action.id]: !state[action.id],
+//             }
+//         }
+//         default:
+//             return state
+//     }
+// }
+
+// export default likeReducer
